ac4cbc43538f16aa3cb4ed4fb14024445ecd9338
BabyDragon003 <golddragon01234@gmail.com>
2019-10-24 15:19:32 +0900

    "2019-10-24 15:19"

--- src/components/CurrencySelect/index.js
+++ src/components/CurrencySelect/index.js
@@ -8,6 +8,17 @@ import { ChevronDown as Arrow } from 'react-feather'
 
 const Select = styled.div`
   position: relative;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  width: fit-content;
+  height: 38px;
+  border-radius: 20px;
+  font-weight: 500;
+  font-size: 1rem;
+  color: ${({ theme }) => theme.textColor};
 
   :hover {
     cursor: pointer;
